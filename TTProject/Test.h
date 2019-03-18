f31b733ade2605a89dc56064cb7df6216c106e29
huweihong <huweihong@miao.cn>
2019-03-18 17:36:55 +0800

    hu_ad

---
+++ TTProject/Test.h
@@ -0,0 +1,20 @@
+//
+//  Test.h
+//  TTProject
+//
+//  Created by huweihong on 2019/3/18.
+//  Copyright © 2019年 huweihong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "LKLdevice1.h"
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Test : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TTProject/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  TTProject
+//
+//  Created by huweihong on 2019/3/18.
+//  Copyright © 2019年 huweihong. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
