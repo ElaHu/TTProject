ee9e9a2412cd7ac768be429792eff77faeea5861
huweihong <huweihong@miao.cn>
2019-04-18 17:33:40 +0800

    hu_ad

--- TTProject/Test.m
+++ TTProject/Test.m
@@ -7,29 +7,10 @@
 //
 
 #import "Test.h"
-#import <TTProject/Coo.h>
 @implementation Test
 
 - (void)showVersion{
     
-    NSString * plistPath = [[NSBundle mainBundle] pathForResource:@"Version" ofType:@"plist"];
-    NSDictionary *dict = [NSDictionary dictionaryWithContentsOfFile:plistPath];
-
-    NSLog(@"------version%@",dict[@"subversion"]);
-    
-    Class clazz = NSClassFromString(@"aaa");
-    
-    NSLog(@"---找到类了%@",clazz);
-
-   
-    Class clazz1 = NSClassFromString(@"Test");
-    
-    NSLog(@"---找到类了111%@",clazz1);
-    
-    Class clazz2 = NSClassFromString(@"Coo");
-//    Coo * object = [[Coo alloc] init];
-    
-    NSLog(@"---找到类了22%@",clazz2);
 
 }
 @end
